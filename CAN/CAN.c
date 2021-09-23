f54d863f8ce00d7c2ee0af01a6918c0fc91ebb4d
chendianyuan <dianyuan.chen@udantech.com>
2021-09-23 18:02:57 +0800

    CAN初版修改

--- CAN/CAN.c
+++ CAN/CAN.c
@@ -13,7 +13,7 @@ void CAN_Init()//CAN初始化
 	}
 	CAN0BTR0_SJW = 0;
 	CAN0BTR0_BRP = 1;
-	CAN0BTR1 = 0x1c;    //比特率:125
+	CAN0BTR1 = 0x1c;	//比特率:125Kbps
 	CAN0IDMR0 = 0xFF;
 	CAN0IDMR1 = 0xFF;
 	CAN0IDMR2 = 0xFF;
@@ -21,7 +21,7 @@ void CAN_Init()//CAN初始化
 	CAN0IDMR4 = 0xFF;
 	CAN0IDMR5 = 0xFF;
 	CAN0IDMR6 = 0xFF;
-	CAN0IDMR7 = 0xFF;
+	CAN0IDMR7 = 0xFF;	//关闭滤波器
 
 	CAN0CTL1 = 0xC0;  //使能MSCAN模块，使用总线时钟源
 	CAN0CTL0 = 0x00;  //返回一般运行模式
@@ -34,7 +34,6 @@ void CAN_Init()//CAN初始化
 		;
 	}
 }
-
 Bool CAN_SendMsg(CanMsg msg)//CAN0发送函数
 {
 	unsigned char send_buf, sp;
@@ -54,14 +53,14 @@ Bool CAN_SendMsg(CanMsg msg)//CAN0发送函数
 	}
 	while (!send_buf);    //寻找空闲的缓冲器
 	CAN0TXIDR0 = (unsigned char)(msg.id >> 21);      //写入标识符
-	CAN0TXIDR1 = (unsigned char)(msg.id >> 13) & 0xE0;
-	CAN0TXIDR1 |= 0x18;
-	CAN0TXIDR1 |= (unsigned char)(msg.id >> 15) & 0x07;
+	CAN0TXIDR1 = (unsigned char)(msg.id >> 13) & 0xE0;//写入CAN0TXIDR1前三位
+	CAN0TXIDR1 |= 0x18;	//写入CAN0TXIDR1中间两位SRR、IDR
+	CAN0TXIDR1 |= (unsigned char)(msg.id >> 15) & 0x07;//写入CAN0TXIDR1后三位
 	CAN0TXIDR2 = (unsigned char)(msg.id >> 7);
 	CAN0TXIDR3 = (unsigned char)(msg.id << 1);
 	if(msg.RTR)
 	{
-		CAN0TXIDR1 |= 0x10; //远程帧
+		CAN0TXIDR3 |= 0x01; //远程帧
 	}
 	for(sp = 0; sp < msg.len; sp++)
 	{
--- CAN/PIT.c
+++ CAN/PIT.c
@@ -5,8 +5,8 @@ void PIT_Init(void)
 {
 	PITCFLMT_PITE = 0;	//关闭PIT
 	PITCE_PCE0 = 1;		//PCE0通道使能
-	PITMUX = 0x00;		//
-	PITMTLD0 = 100 - 1;	//使用微定时器0计数
+	PITMUX = 0x00;		//使用微定时器0计数
+	PITMTLD0 = 100 - 1;
 	PITLD0 = 4000 - 1;	//T = (PITMTLD+1)*(PITLD+1)/Fbus = 100ms
 	PITINTE_PINTE0 = 1;	//使能PIT
 	PITCFLMT_PITE = 1;
